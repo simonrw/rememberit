13d531dba4a7f51ad1ecebaaeb290f6b065f2813
Simon Walker <s.r.walker101@googlemail.com>
2023-02-28 11:47:50 +0000

    Include flake for packages

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Flake utils demo";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [
+        ];
+
+        pkgs = import nixpkgs {
+          inherit overlays system;
+        };
+      in
+      {
+        devShells = rec {
+          default = empty;
+
+          empty = pkgs.mkShell {
+            buildInputs = with pkgs; [
+            ];
+          };
+        };
+      }
+    );
+}
