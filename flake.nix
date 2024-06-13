7ca7e89f428bcbac89d582c8ce109b59919947c6
Simon Walker <s.r.walker101@googlemail.com>
2024-06-13 21:33:09 +0100

    Use playwright browsers from nixpkgs

--- flake.nix
+++ flake.nix
@@ -15,13 +15,6 @@
           inherit overlays system;
           config.allowUnfree = true;
         };
-
-        playwright-browsers = import ./nix/playwright-browsers.nix {
-          inherit pkgs;
-          playwright_version = "1.22.0";
-          data_sha256 = "sha256:1jbq5xdklw2n8cqxjv912q124wmlqkwv6inlf6qw76x9ns16lv18";
-        };
-
       in
       {
         devShells = rec {
@@ -32,7 +25,10 @@
               playwright
             ];
 
-            PLAYWRIGHT_CHROMIUM = playwright-browsers.chromium;
+            env = {
+              PLAYWRIGHT_BROWSERS_PATH = pkgs.playwright-driver.browsers;
+              PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS = true;
+            };
           };
         };
       }
