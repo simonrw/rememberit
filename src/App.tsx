60864485c6fe5863cbf6e7c5b47f5b37fbf37dcd
Simon Walker <s.r.walker101@googlemail.com>
2024-07-03 17:57:19 +0100

    Add entry view

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,24 @@
+type Item = {
+  content: string;
+  created: Date;
+};
+
+const items: Item[] = [
+  {
+    content: "First item",
+    created: new Date(),
+  },
+  {
+    content: "Second item",
+    created: new Date(),
+  },
+  {
+    content: "Third item",
+    created: new Date(),
+  },
+];
+
+
 function App() {
   return (
     <main className="flex flex-col gap-4 p-4 h-screen w-screen">
@@ -12,8 +33,22 @@ function App() {
         <input type="text" id="entry-input" className="flex-1 border border-black rounded-sm" autoFocus={true}></input>
         <button className="rounded-2xl bg-blue-500 px-4 py-2 text-white">Add entry</button>
       </div>
+      <div className="flex flex-col overflow-y-scroll">
+        {[...items, ...items, ...items, ...items].map((item) => {
+          return Entry(item);
+        })}
+      </div>
     </main>
   );
+}
+
+function Entry(entry: Item) {
+    return (
+      <div className="flex flex-1 gap-2">
+        <span className="text-gray-500 font-light">{entry.created.toLocaleString()}</span>
+        <span>{entry.content}</span>
+      </div>
+    );
 }
 
 export default App;
