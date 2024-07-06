c9bcc1b69ae8a6d02489cdbe09e61c4f8680bdc1
Simon Walker <s.r.walker101@googlemail.com>
2024-07-06 21:19:09 +0100

    Add structure

--- src/components/EntryList.tsx
+++ src/components/EntryList.tsx
@@ -1,16 +1,15 @@
 import { Entry, Item } from "./Entry";
 
 export interface EntryListProps {
-  items: Item[];
-  deleteFn: (id: string) => void;
+ items: Item[];
+  deleteFn: (id: string) => void ;
 }
 
 export function EntryList(props: EntryListProps) {
-  return (
-    <div className="flex flex-col overflow-y-scroll">
-      {props.items.map((item) => {
-        return Entry({ item, deleteFn: props.deleteFn });
-      })}
-    </div>
-  );
+  return (<div className="flex flex-col overflow-y-scroll">
+    {props.items.map((item) => {
+      return Entry({ item, deleteFn: props.deleteFn });
+    })}
+  </div>
+  )
 }
