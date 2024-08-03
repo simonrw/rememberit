7c4c0428f8c456d3b9f3c4ce05d9162029944534
Simon Walker <s.r.walker101@googlemail.com>
2024-08-03 22:16:13 +0100

    Add PWA support (#21)

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Item } from "./components/Entry";
 import { EntryList } from "./components/EntryList";
 import QuickAdds from "./components/QuickAdds";
 import { newDate } from "./date";
+import { Footer } from "./components/Footer";
 
 const STORAGE_ITEM_KEY = "entries";
 
@@ -66,109 +67,112 @@ function Content() {
   };
 
   return (
-    <main className="dark:bg-gray-800 dark:text-white flex flex-col gap-4 p-4 h-screen w-screen">
-      <h1 className="text-2xl font-bold">RememberIt</h1>
-      <div className="flex gap-2 justify-start">
-        {/* Reset entries */}
-        <button
-          className="rounded-2xl bg-blue-500 px-4 py-2 text-white"
-          onClick={resetItems}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-6"
+    <div className="h-screen flex flex-col">
+      <main className="dark:bg-gray-800 dark:text-white flex flex-col gap-4 p-4 w-screen flex-1 overflow-y-auto">
+        <h1 className="text-2xl font-bold">RememberIt</h1>
+        <div className="flex gap-2 justify-start">
+          {/* Reset entries */}
+          <button
+            className="rounded-2xl bg-blue-500 px-4 py-2 text-white"
+            onClick={resetItems}
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-            />
-          </svg>
-        </button>
-        {/* export state */}
-        <button
-          className="rounded-2xl bg-blue-500 px-4 py-2 text-white"
-          onClick={exportState}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-6"
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+              />
+            </svg>
+          </button>
+          {/* export state */}
+          <button
+            className="rounded-2xl bg-blue-500 px-4 py-2 text-white"
+            onClick={exportState}
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M19.5 13.5 12 21m0 0-7.5-7.5M12 21V3"
-            />
-          </svg>
-        </button>
-        {/* import state */}
-        <button className="rounded-2xl bg-blue-500 px-4 py-2 text-white">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M4.5 10.5 12 3m0 0 7.5 7.5M12 3v18"
-            />
-          </svg>
-        </button>
-      </div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M19.5 13.5 12 21m0 0-7.5-7.5M12 21V3"
+              />
+            </svg>
+          </button>
+          {/* import state */}
+          <button className="rounded-2xl bg-blue-500 px-4 py-2 text-white">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M4.5 10.5 12 3m0 0 7.5 7.5M12 3v18"
+              />
+            </svg>
+          </button>
+        </div>
 
-      <QuickAdds entries={items} addEntry={addItem} />
+        <QuickAdds entries={items} addEntry={addItem} />
 
-      <form className="flex gap-2 items-center"
-        onSubmit={(e) => {
-          e.preventDefault();
-          addItem(newText);
-          setNewText("");
-        }}
-      >
-        <label htmlFor="entry-input">Entry</label>
-        <input
-          type="text"
-          id="entry-input"
-          value={newText}
-          className="flex-1 border border-black rounded-sm dark:text-black"
-          autoFocus={true}
-          onChange={(e) => {
-            setNewText(e.target.value);
+        <form className="flex gap-2 items-center"
+          onSubmit={(e) => {
+            e.preventDefault();
+            addItem(newText);
+            setNewText("");
           }}
-        ></input>
-        <button
-          className="rounded-2xl bg-blue-500 px-4 py-2 text-white"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-6"
+          <label htmlFor="entry-input">Entry</label>
+          <input
+            type="text"
+            id="entry-input"
+            value={newText}
+            className="flex-1 border border-black rounded-sm dark:text-black"
+            autoFocus={true}
+            onChange={(e) => {
+              setNewText(e.target.value);
+            }}
+          ></input>
+          <button
+            className="rounded-2xl bg-blue-500 px-4 py-2 text-white"
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-            />
-          </svg>
-        </button>
-      </form>
-      <EntryList items={items} deleteFn={deleteItem} updateFn={updateItem} />
-    </main>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+              />
+            </svg>
+          </button>
+        </form>
+        <EntryList items={items} deleteFn={deleteItem} updateFn={updateItem} />
+      </main>
+      <Footer />
+    </div>
   );
 }
 
---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+  return (
+    <footer className="p-2 bg-gray-800 text-gray-500 font-thin text-xs">
+      <a href="https://www.flaticon.com/free-icons/remember" title="remember icons">Remember icons created by Freepik - Flaticon</a>
+    </footer>
+  );
+}
