7df5c62a574d2422aa06371668da8918891485a2
Simon Walker <s.r.walker101@googlemail.com>
2024-08-04 21:07:11 +0100

    Set up vite-pwa plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,28 @@ import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 import react from "@vitejs/plugin-react";
 
+const vitePWA = VitePWA({
+  manifest: {
+    name: "RememberIt",
+    short_name: "RememberIt",
+    start_url: "/",
+    background_color: "#1f2937",
+    theme_color: "#4b5563",
+    icons: [
+      {
+        src: "icons/remembering.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+    display: "standalone",
+  },
+  devOptions: {
+    enabled: true,
+  },
+});
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA()],
+  plugins: [react(), vitePWA],
 });
