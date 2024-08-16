ae6a0854c8c846fbaa264f8e9bd5f15c9c6aa547
Simon Walker <s.r.walker101@googlemail.com>
2024-08-16 17:06:55 +0100

    fix: incorrect timezone for edits (#37)
    When editing items, the date would be wrong. This is due to the
    incorrect timezone being used.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,12 @@
 import { type ClassValue, clsx } from "clsx";
+import moment, { Moment } from "moment-timezone";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function momentToDatePicker(date: Moment): string {
+  const tz = moment.tz.guess();
+  return date.tz(tz).format("YYYY-MM-DDTHH:mm");
 }
